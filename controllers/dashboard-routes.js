b106e5157e84ccd137c3169457c4a97ad716d614
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-06-20 13:31:11 -0500

    fix errors

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -10,7 +10,7 @@ router.get('/', (req, res) => {
       },
       attributes: [
         'id',
-        'post_url',
+        'post_content',
         'title',
         'created_at',
       ],
--- server.js
+++ server.js
@@ -4,8 +4,7 @@ const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
-const helpers = require('./utils/helpers');
-const hbs = exphbs.create({helpers});
+const hbs = exphbs.create({});
 
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
