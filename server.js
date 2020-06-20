8b16cb83b5aef40e5e73567ffe2c5ac5d6984548
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-06-20 13:11:49 -0500

    add homepage.handlebars file

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({helpers});
 
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -25,18 +28,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session(sess));
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 // turn on routes
 app.use(routes);
 
-app.use(session(sess));
-
 // turn on connection to db and server
-// sync means that this is Sequelize taking the models and connecting them to associated database tables... if it doesn't find a table it will create it for you!
-// {force: false} doesn't have to be included but if set to true, it would drop the re-create all of databases tables on startup which is great for when we need to make changes to the Sequelize models 
-// think of it like DROP TABLE IF EXISTS
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
