099da0d6495ce90ebb9b4832d4dd2efe74c57ed9
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-06-21 12:19:23 -0500

    delete email field, not needed and was getting validation errors

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
+const withAuth = require('../../utils/auth');
 
 // GET /api/posts
 router.get('/', (req, res) => {
@@ -65,7 +66,7 @@ router.get('/:id', (req, res) => {
     });
 });
 
-  router.post('/', (req, res) => {
+  router.post('/', withAuth, (req, res) => {
     Post.create({
       title: req.body.title,
       post_content: req.body.post_content,
@@ -79,11 +80,11 @@ router.get('/:id', (req, res) => {
   });
 
 
-  router.put('/:id', (req, res) => {
+  router.put('/:id', withAuth, (req, res) => {
     Post.update(
       {
         title: req.body.title,
-        post_content: req.body.post_content
+        // post_content: req.body.post_content
       },
       {
         where: {
